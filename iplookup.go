0b52eaa5c3e8ebfea6b12c70721550d157ff2236
kkou <a@b.c>
2022-04-29 09:47:20 +0800

    update param description

--- iplookup.go
+++ iplookup.go
@@ -24,7 +24,7 @@ func main() {
 			&cli.StringFlag{
 				Name:    "file",
 				Aliases: []string{"f"},
-				Usage:   "file name for ips",
+				Usage:   "file name for IP list",
 			},
 			&cli.StringFlag{
 				Name:    "language",
